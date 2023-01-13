685af014cada1f87af87aef4a189ce1ad9ff5686
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-13 18:55:47 +0700

    i fink it's done. YES!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         MyDecimalFormat myDecimalFormat = new MyDecimalFormat(1234567);
+        System.out.println(myDecimalFormat.getInputInt());
         myDecimalFormat.printInFormat();
     }
 }
